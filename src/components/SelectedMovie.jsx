97b01ade0b5b254da9b59f939310f5f25a6085a3
Sreesanth-01 <iamshanth7@gmail.com>
2025-12-09 13:44:27 +0530

    Improved css for click

--- src/components/SelectedMovie.jsx
+++ src/components/SelectedMovie.jsx
@@ -4,13 +4,15 @@ const SelectedMovie = ({movie,onClose}) =>{
     }
     const {title,poster_path,overview} = movie
     return(
-        <div className="selected-card">
+        <div className="selected-backdrop">
             <div className="modal">
                 <button className="close-btn" onClick={onClose}>✕</button>
 
                 <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : 'no-movie.png' } alt={title} />
-                <h2>{title}</h2>
-                <p className="overview">{overview}</p>
+                <div className="model-content">
+                    <h2>{title}</h2>
+                    <p className="overview">{overview}</p>
+                </div>
 
             </div>
 
