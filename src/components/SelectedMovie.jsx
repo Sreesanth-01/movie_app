365ce8c7c254e89a2489a592f6dec025a2a0d344
Sreesanth-01 <iamshanth7@gmail.com>
2025-12-09 13:52:10 +0530

    CSS fix

--- src/components/SelectedMovie.jsx
+++ src/components/SelectedMovie.jsx
@@ -2,7 +2,7 @@ const SelectedMovie = ({movie,onClose}) =>{
     if(!movie){
         return null
     }
-    const {title,poster_path,overview} = movie
+    const {title,poster_path,overview,vote_average,original_language,release_date} = movie
     return(
         <div className="selected-backdrop">
             <div className="modal">
@@ -11,6 +11,14 @@ const SelectedMovie = ({movie,onClose}) =>{
                 <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : 'no-movie.png' } alt={title} />
                 <div className="model-content">
                     <h2>{title}</h2>
+                    <div className="movie-meta">
+                        
+                        <span>⭐ {movie.vote_average.toFixed(1)}</span>
+                         <span>•</span>
+                        <p className='lang'>{original_language}</p>
+                        <span>•</span>
+                        <p className='year'>{release_date ? release_date.split('-')[0] : 'N/A'}</p>
+                    </div>
                     <p className="overview">{overview}</p>
                 </div>
 
